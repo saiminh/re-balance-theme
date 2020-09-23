66928e5d53d14c89cea2c79de03a3eec1eac2971
Eros Milfton <simon@floter.design>
2020-09-23 16:05:25 +0200

    try this

--- page-test.php
+++ page-test.php
@@ -18,10 +18,12 @@ get_header();
             $member_id = SwpmMemberUtils::get_logged_in_members_id();
             $swpm_user = SwpmMemberUtils::get_user_by_id($member_id);
             //var_dump($swpm_user);
-            $txn_id = SwpmAuth::get_instance()->txn_id;
-            var_dump(SwpmAuth::get_instance());
+            $subscr_id = SwpmAuth::get_instance()->subscr_id;
+            $transaction = SwpmTransactions::get_transaction_row_by_subscr_id($subscr_id);
+            var_dump($transaction);
+            //var_dump($subscr_id);
             
-            if ($txn_id) {
+            if ($subscr_id) {
               //composer require stripe/stripe-php;
               require_once('stripe-php/init.php');
               // Set your secret key. Remember to switch to your live secret key in production!
@@ -29,7 +31,7 @@ get_header();
               \Stripe\Stripe::setApiKey('TESTSTRIPEAPIKEY');
 
               $stripecall = \Stripe\BillingPortal\Session::create([
-                'customer' => $txn_id,
+                'customer' => $subscr_id,
                 'return_url' => 'http://rebalance.local/membership-profile',
               ]);
               echo '<a class="button" href="'.$stripecall['url'].'">Stripe</a>';
