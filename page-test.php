8770b8cb2b0053ae26f64351bd202ad3d7dbbc72
Eros Milfton <simon@floter.design>
2020-09-23 15:46:00 +0200

    txn id instead

--- page-test.php
+++ page-test.php
@@ -18,10 +18,10 @@ get_header();
             $member_id = SwpmMemberUtils::get_logged_in_members_id();
             $swpm_user = SwpmMemberUtils::get_user_by_id($member_id);
             //var_dump($swpm_user);
-            $subscr_id = SwpmAuth::get_instance()->subscr_id;
-            var_dump($subscr_id);
+            $txn_id = SwpmAuth::get_instance()->txn_id;
+            var_dump(SwpmAuth::get_instance());
             
-            if ($subscr_id) {
+            if ($txn_id) {
               //composer require stripe/stripe-php;
               require_once('stripe-php/init.php');
               // Set your secret key. Remember to switch to your live secret key in production!
@@ -29,7 +29,7 @@ get_header();
               \Stripe\Stripe::setApiKey('TESTSTRIPEAPIKEY');
 
               $stripecall = \Stripe\BillingPortal\Session::create([
-                'customer' => $subscr_id,
+                'customer' => $txn_id,
                 'return_url' => 'http://rebalance.local/membership-profile',
               ]);
               echo '<a class="button" href="'.$stripecall['url'].'">Stripe</a>';
