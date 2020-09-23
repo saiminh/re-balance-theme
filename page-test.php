394f9a0fdd8b1a276f404bffc3be6a4c5b683a7c
Eros Milfton <simon@floter.design>
2020-09-23 16:27:52 +0200

    txnid

--- page-test.php
+++ page-test.php
@@ -129,26 +129,26 @@ get_header();
             $member_id = SwpmMemberUtils::get_logged_in_members_id();
             $swpm_user = SwpmMemberUtils::get_user_by_id($member_id);
             //var_dump($swpm_user);
-            $subscr_id = SwpmAuth::get_instance()->userData->subscr_id;
+            $subscr_id = SwpmAuth::get_instance()->userData->txn_id;
             //$transaction = SwpmTransactions::get_transaction_row_by_subscr_id($subscr_id);
             var_dump($subscr_id);
             echo $subscr_id;
            // var_dump(SwpmAuth::get_instance());
             //var_dump($subscr_id);
             
-            if ($subscr_id) {
-              //composer require stripe/stripe-php;
-              require_once('stripe-php/init.php');
-              // Set your secret key. Remember to switch to your live secret key in production!
-              // See your keys here: https://dashboard.stripe.com/account/apikeys
-              \Stripe\Stripe::setApiKey('TESTSTRIPEAPIKEY');
+            // if ($subscr_id) {
+            //   //composer require stripe/stripe-php;
+            //   require_once('stripe-php/init.php');
+            //   // Set your secret key. Remember to switch to your live secret key in production!
+            //   // See your keys here: https://dashboard.stripe.com/account/apikeys
+            //   \Stripe\Stripe::setApiKey('TESTSTRIPEAPIKEY');
 
-              $stripecall = \Stripe\BillingPortal\Session::create([
-                'customer' => $subscr_id,
-                'return_url' => 'http://rebalance.local/membership-profile',
-              ]);
-              echo '<a class="button" href="'.$stripecall['url'].'">Stripe</a>';
-            }
+            //   $stripecall = \Stripe\BillingPortal\Session::create([
+            //     'customer' => $subscr_id,
+            //     'return_url' => 'http://rebalance.local/membership-profile',
+            //   ]);
+            //   echo '<a class="button" href="'.$stripecall['url'].'">Stripe</a>';
+            // }
           ?>
           
         </div>
