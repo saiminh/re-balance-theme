149eb1cc745dc7e5e1184d9bfb151fd0f19f63ef
Eros Milfton <simon@floter.design>
2020-09-23 15:33:19 +0200

    testing swpm customer portal link

--- page-test.php
+++ page-test.php
@@ -13,23 +13,29 @@ get_header();
 		<main id="main" class="site-main">
       <article>
         <div>
-          <form method="POST" action="/create_customer_portal_session">
-              <button type="submit">Manage billing</button>
-          </form>
-    
           <?php 
-              
-            //composer require stripe/stripe-php;
-            require_once('stripe-php/init.php');
-            // Set your secret key. Remember to switch to your live secret key in production!
-            // See your keys here: https://dashboard.stripe.com/account/apikeys
-            \Stripe\Stripe::setApiKey('TESTSTRIPEAPIKEY');
+            
+            $member_id = SwpmMemberUtils::get_logged_in_members_id();
+            $swpm_user = SwpmMemberUtils::get_user_by_id($member_id);
+            //var_dump($swpm_user);
+            $subscr_id = SwpmAuth::get_instance()->subscr_id;
+            //var_dump($subscr_id);
+            
+            if ($subscr_id) {
+              //composer require stripe/stripe-php;
+              require_once('stripe-php/init.php');
+              // Set your secret key. Remember to switch to your live secret key in production!
+              // See your keys here: https://dashboard.stripe.com/account/apikeys
+              \Stripe\Stripe::setApiKey('TESTSTRIPEAPIKEY');
 
-            \Stripe\BillingPortal\Session::create([
-              'customer' => 'cus_I3tmmPcMtQSWOB',
-              'return_url' => 'https://re-balace.io/account',
-            ]);
+              $stripecall = \Stripe\BillingPortal\Session::create([
+                'customer' => $subscr_id,
+                'return_url' => 'http://rebalance.local/membership-profile',
+              ]);
+              echo '<a class="button" href="'.$stripecall['url'].'">Stripe</a>';
+            }
           ?>
+          
         </div>
       </article>
 		</main><!-- #main -->
