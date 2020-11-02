544d1f7fdcf49d288c79b955d08a7849bbf345c8
Eros Milfton <simon@floter.design>
2020-11-02 13:46:13 +0100

    favicons

--- header.php
+++ header.php
@@ -19,6 +19,9 @@
 	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff9b7a">
+	<meta name="msapplication-TileColor" content="#ff9b7a">
+	<meta name="theme-color" content="#ffffff">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
