19647b5a858b0506148851c86c8c3cccb81c761a
saiminh <saiminh@gmail.com>
2022-06-24 14:19:58 +0200

    maintenance mode

--- functions.php
+++ functions.php
@@ -830,5 +830,12 @@ function after_registration_callback ($member_info)
 }
 add_action('swpm_front_end_registration_complete_user_data', 'after_registration_callback');
 
+function wp_maintenance_mode() {
+  if (!current_user_can('edit_themes') || !is_user_logged_in()) {
+    wp_die('<h1>Rebalance is now offline</h1>');
+  }
+}
+add_action('get_header', 'wp_maintenance_mode');
+
 ?>
 
